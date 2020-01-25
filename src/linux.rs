7261b847646593362c0d92597e9460e14fee163a
A. Cody Schuffelen <schuffelen@google.com>
2020-01-24 17:19:11 -0800

    Revert "Small changes to build crosvm.experimental"
    This reverts commit 38963cebe10644d12c346c219872652d82549d9b.
    
    Bug: 140069322
    Test: Later changes compile
    Change-Id: I5dd73a8e52ba45eda7ba01fbc827084533641964

--- src/linux.rs
+++ src/linux.rs
@@ -4,7 +4,7 @@
 
 use std;
 use std::cmp::min;
-use std::convert::{TryFrom, TryInto};
+use std::convert::TryFrom;
 use std::error::Error as StdError;
 use std::ffi::CStr;
 use std::fmt::{self, Display};
@@ -320,7 +320,7 @@ fn create_base_minijail(
     // Run in an empty network namespace.
     j.namespace_net();
     // Most devices don't need to open many fds.
-    j.set_rlimit(libc::RLIMIT_NOFILE.try_into().unwrap(), 1024, 1024)
+    j.set_rlimit(libc::RLIMIT_NOFILE, 1024, 1024)
         .map_err(Error::SettingMaxOpenFiles)?;
     // Apply the block device seccomp policy.
     j.no_new_privs();
@@ -796,7 +796,7 @@ fn create_fs_device(
 
     // The file server opens a lot of fds and needs a really high open file limit.
     let max_open_files = get_max_open_files()?;
-    j.set_rlimit(libc::RLIMIT_NOFILE.try_into().unwrap(), max_open_files, max_open_files)
+    j.set_rlimit(libc::RLIMIT_NOFILE, max_open_files, max_open_files)
         .map_err(Error::SettingMaxOpenFiles)?;
 
     // TODO(chirantan): Use more than one worker once the kernel driver has been fixed to not panic
